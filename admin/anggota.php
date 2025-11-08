aa898aaeeb8989d04f0cceccc815f220f2920ba1
monstaxrosie <sukacintazoro@gmail.com>
2025-11-08 13:47:08 +0700

    crud anggota

---
+++ admin/anggota.php
@@ -0,0 +1,136 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Studiova</title>
+  <link rel="shortcut icon" type="image/png" href="./template/assets/images/logos/favicon.svg" />
+  <link rel="stylesheet" href="./template/assets/libs/owl.carousel/dist/assets/owl.carousel.min.css">
+  <link rel="stylesheet" href="./template/assets/libs/aos-master/dist/aos.css">
+  <link rel="stylesheet" href="./template/assets/css/styles.css" />
+</head>
+
+<body>
+
+  <!--  Page Wrapper -->
+  <div class="page-wrapper overflow-hidden">
+
+    <!--  Get in touch Section -->
+    <section
+      class="bg-light-gray border-top border-primary border-4 d-flex align-items-center justify-content-center min-vh-100">
+      <div class="container py-3">
+        <div class="sign-in card mx-auto shadow-lg">
+          <div class="card-body py-8 px-lg-5">
+            <a href="index.html" class="mb-8 hstack justify-content-center">
+              <img src="./template/assets/images/logos/logo-dark.svg" alt="logo-dark" class="img-fluid">
+            </a>
+            <form class="d-flex flex-column gap-3">
+              <div>
+                <input type="text" name="username" class="form-control border-bottom" placeholder="Username">
+              </div>
+              <div>
+                <input type="password" name="username" class="form-control border-bottom" placeholder="Password">
+              </div>
+
+              <a href="index.html" class="btn btn-dark w-100 justify-content-center py-2 fw-medium my-7 fs-4 lh-lg">
+                Log in
+              </a>
+            </form>
+            <a class="text-center mb-1 d-block text-dark fw-medium" href="#">Forget Password?</a>
+            <p class="mb-0 fw-medium text-center">Not a member yet? <a class="text-dark" href="sign-up.html">Regristasi</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+  </div>
+
+  <div class="get-template hstack gap-2">
+    
+    <button class="btn bg-primary p-2 round-52 rounded-circle hstack justify-content-center flex-shrink-0"
+      id="scrollToTopBtn">
+      <iconify-icon icon="lucide:arrow-up" class="fs-7 text-dark"></iconify-icon>
+    </button>
+  </div>
+
+
+  <script src="./template/assets/libs/jquery/dist/jquery.min.js"></script>
+  <script src="./template/assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="./template/assets/libs/owl.carousel/dist/owl.carousel.min.js"></script>
+  <script src="./template/assets/libs/aos-master/dist/aos.js"></script>
+  <script src="./template/assets/js/custom.js"></script>
+  <!-- solar icons -->
+  <script src="https://cdn.jsdelivr.net/npm/iconify-icon@1.0.8/dist/iconify-icon.min.js"></script>
+</body>
+
+</html>
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Studiova</title>
+  <link rel="shortcut icon" type="image/png" href="./template/assets/images/logos/favicon.svg" />
+  <link rel="stylesheet" href="./template/assets/libs/owl.carousel/dist/assets/owl.carousel.min.css">
+  <link rel="stylesheet" href="./template/assets/libs/aos-master/dist/aos.css">
+  <link rel="stylesheet" href="./template/assets/css/styles.css" />
+</head>
+
+<body>
+
+  <!--  Page Wrapper -->
+  <div class="page-wrapper overflow-hidden">
+
+    <!--  Get in touch Section -->
+    <section
+      class="bg-light-gray border-top border-primary border-4 d-flex align-items-center justify-content-center min-vh-100">
+      <div class="container py-3">
+        <div class="sign-in card mx-auto shadow-lg">
+          <div class="card-body py-8 px-lg-5">
+            <a href="index.html" class="mb-8 hstack justify-content-center">
+              <img src="./template/assets/images/logos/logo-dark.svg" alt="logo-dark" class="img-fluid">
+            </a>
+            <form class="d-flex flex-column gap-3">
+              <div>
+                <input type="text" name="username" class="form-control border-bottom" placeholder="Username">
+              </div>
+              <div>
+                <input type="password" name="username" class="form-control border-bottom" placeholder="Password">
+              </div>
+
+              <a href="index.html" class="btn btn-dark w-100 justify-content-center py-2 fw-medium my-7 fs-4 lh-lg">
+                Log in
+              </a>
+            </form>
+            <a class="text-center mb-1 d-block text-dark fw-medium" href="#">Forget Password?</a>
+            <p class="mb-0 fw-medium text-center">Not a member yet? <a class="text-dark" href="sign-up.html">Regristasi</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+  </div>
+
+  <div class="get-template hstack gap-2">
+    
+    <button class="btn bg-primary p-2 round-52 rounded-circle hstack justify-content-center flex-shrink-0"
+      id="scrollToTopBtn">
+      <iconify-icon icon="lucide:arrow-up" class="fs-7 text-dark"></iconify-icon>
+    </button>
+  </div>
+
+
+  <script src="./template/assets/libs/jquery/dist/jquery.min.js"></script>
+  <script src="./template/assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="./template/assets/libs/owl.carousel/dist/owl.carousel.min.js"></script>
+  <script src="./template/assets/libs/aos-master/dist/aos.js"></script>
+  <script src="./template/assets/js/custom.js"></script>
+  <!-- solar icons -->
+  <script src="https://cdn.jsdelivr.net/npm/iconify-icon@1.0.8/dist/iconify-icon.min.js"></script>
+</body>
+
+</html>
---
+++ angoota/home.php
@@ -0,0 +1,212 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Skydash Admin</title>
+  <!-- plugins:css -->
+  <link rel="stylesheet" href="../template2/vendors/feather/feather.css">
+  <link rel="stylesheet" href="../template2/vendors/ti-icons/css/themify-icons.css">
+  <link rel="stylesheet" href="../template2/vendors/css/vendor.bundle.base.css">
+  <!-- endinject -->
+  <!-- Plugin css for this page -->
+  <link rel="stylesheet" href="../template2/vendors/datatables.net-bs4/dataTables.bootstrap4.css">
+  <link rel="stylesheet" href="../template2/vendors/ti-icons/css/themify-icons.css">
+  <link rel="stylesheet" type="../template2/text/css" href="../template2/js/select.dataTables.min.css">
+  <!-- End plugin css for this page -->
+  <!-- inject:css -->
+  <link rel="stylesheet" href="../template2/css/vertical-layout-light/style.css">
+  <!-- endinject -->
+  <link rel="shortcut icon" href="../template2/images/favicon.png" />
+</head>
+<body>
+  <div class="container-scroller">
+    <!-- partial:partials/_navbar.html -->
+    <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
+      <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
+        <a class="navbar-brand brand-logo mr-5" href="index.html"><img src="../template2/images/logo.svg" class="mr-2" alt="logo"/></a>
+        <a class="navbar-brand brand-logo-mini" href="index.html"><img src="../template2/images/logo-mini.svg" alt="logo"/></a>
+      </div>
+      <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
+        <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
+          <span class="icon-menu"></span>
+        </button>
+        <ul class="navbar-nav navbar-nav-right">
+          <li class="nav-item nav-profile dropdown">
+            <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" id="profileDropdown">
+              <img src="../template2/images/faces/face28.jpg" alt="profile"/>
+            </a>
+            <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="profileDropdown">
+              <a class="dropdown-item">
+                <i class="ti-settings text-primary"></i>
+                Settings
+              </a>
+              <a class="dropdown-item">
+                <i class="ti-power-off text-primary"></i>
+                Logout
+              </a>
+            </div>
+          </li>    
+        </ul>
+        <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
+          <span class="icon-menu"></span>
+        </button>
+      </div>
+    </nav>
+    <!-- partial -->
+    <div class="container-fluid page-body-wrapper">
+      <!-- partial:partials/_settings-panel.html -->
+      <div class="theme-setting-wrapper">
+        <div id="settings-trigger"><i class="ti-settings"></i></div>
+        <div id="theme-settings" class="settings-panel">
+          <i class="settings-close ti-close"></i>
+          <p class="settings-heading">SIDEBAR SKINS</p>
+          <div class="sidebar-bg-options selected" id="sidebar-light-theme"><div class="img-ss rounded-circle bg-light border mr-3"></div>Light</div>
+          <div class="sidebar-bg-options" id="sidebar-dark-theme"><div class="img-ss rounded-circle bg-dark border mr-3"></div>Dark</div>
+          <p class="settings-heading mt-2">HEADER SKINS</p>
+          <div class="color-tiles mx-0 px-4">
+            <div class="tiles success"></div>
+            <div class="tiles warning"></div>
+            <div class="tiles danger"></div>
+            <div class="tiles info"></div>
+            <div class="tiles dark"></div>
+            <div class="tiles default"></div>
+          </div>
+        </div>
+      </div>
+      <!-- partial:setting_pannel -->
+
+      <!-- sidebar -->
+       <?php
+      include '../layout/sidebar.php';
+      ?>
+      <!-- sidebar -->
+
+    <!-- partial -->
+      <div class="main-panel">
+        <div class="content-wrapper">
+          <div class="row">
+            <div class="col-md-12 grid-margin">
+              <div class="row">
+                <div class="col-12 col-xl-8 mb-4 mb-xl-0">
+                  <h1 class="font-weight-bold">Welcome</h1>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-6 grid-margin stretch-card">
+              <div class="card tale-bg">
+                <div class="card-people mt-auto">
+                  <img src="../template2/images/dashboard/people.svg" alt="people">
+                  <div class="weather-info">
+                    <div class="d-flex">
+                      <div>
+                        <h2 class="mb-0 font-weight-normal"><i class="icon-sun mr-2"></i>31<sup>C</sup></h2>
+                      </div>
+                      <div class="ml-2">
+                        <h4 class="location font-weight-normal"><b>Kajen</b></h4>
+                        <h6 class="font-weight-normal">Mrgoyoso</h6>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6 grid-margin transparent">
+              <div class="row">
+                <div class="col-md-6 mb-4 stretch-card transparent">
+                  <div class="card card-tale">
+                    <div class="card-body">
+                      <?php
+                        require_once '../koneksi.php';
+                        $query_total = "SELECT COUNT(*) as total, SUM(nominal) as total_saldo FROM simpanan";
+                        $result_total = mysqli_query($koneksi, $query_total);
+                        $data_total = mysqli_fetch_assoc($result_total);
+
+
+                        $query_jumlah_pinjaman_acc= mysqli_query($koneksi, "select sum(jumlah_pinjaman) as total from pinjaman where status='acc'");
+                        $jumlah_pinjaman_acc = mysqli_fetch_assoc($query_jumlah_pinjaman_acc);
+                      ?>
+                      <p class="card-title text-white mb-4">Total Saldo</p>                      
+                      <p class="fs-30 mb-2">
+                        Rp <?php echo number_format($data_total['total_saldo'], 0, ',', '.'); ?>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-6 mb-4 stretch-card transparent">
+                  <div class="card card-dark-blue">
+                    <div class="card-body">
+                      <p class="mb-4"><b>Total Pinjaman</b></p>
+                      <p class="fs-30 mb-2"><?="Rp ".number_format($jumlah_pinjaman_acc['total'],2,',','.')?></p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-6 mb-4 mb-lg-0 stretch-card transparent">
+                  <div class="card card-light-blue">
+                    <div class="card-body">
+                      <p class="mb-4"><b>Total angsuran</b></p>
+                      <p class="fs-30 mb-2">0000</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-6 stretch-card transparent">
+                  <div class="card card-light-danger">
+                    <div class="card-body">
+                      <p class="mb-4"><b>Total Anggota</b></p>
+                      <p class="fs-30 mb-2">0000</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        <!-- content-wrapper ends -->
+        <!-- partial:partials/_footer.html -->
+        <footer class="footer">
+          <div class="d-sm-flex justify-content-center justify-content-sm-between">
+            <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2021.  Premium <a href="https://www.bootstrapdash.com/" target="_blank">Bootstrap admin template</a> from BootstrapDash. All rights reserved.</span>
+            <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Hand-crafted & made with <i class="ti-heart text-danger ml-1"></i></span>
+          </div>
+          <div class="d-sm-flex justify-content-center justify-content-sm-between">
+            <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Distributed by <a href="https://www.themewagon.com/" target="_blank">Themewagon</a></span> 
+          </div>
+        </footer> 
+        <!-- partial -->
+      </div>
+      <!-- main-panel ends -->
+    </div>   
+    <!-- page-body-wrapper ends -->
+  </div>
+  <!-- container-scroller -->
+
+  <!-- plugins:js -->
+  <script src="../template2/vendors/js/vendor.bundle.base.js"></script>
+  <!-- endinject -->
+  <!-- Plugin js for this page -->
+  <script src="../template2/vendors/chart.js/Chart.min.js"></script>
+  <script src="../template2/vendors/datatables.net/jquery.dataTables.js"></script>
+  <script src="../template2/vendors/datatables.net-bs4/dataTables.bootstrap4.js"></script>
+  <script src="../template2/js/dataTables.select.min.js"></script>
+
+  <!-- End plugin js for this page -->
+  <!-- inject:js -->
+  <script src="../template2/js/off-canvas.js"></script>
+  <script src="../template2/js/hoverable-collapse.js"></script>
+  <script src="../template2/js/template.js"></script>
+  <script src="../template2/js/settings.js"></script>
+  <script src="../template2/js/todolist.js"></script>
+  <!-- endinject -->
+  <!-- Custom js for this page-->
+  <script src="../template2/js/dashboard.js"></script>
+  <script src="../template2/js/Chart.roundedBarCharts.js"></script>
+  <!-- End custom js for this page-->
+</body>
+
+</html>
+
